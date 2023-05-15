0f5ae14de6ee6e070a0929a95cf68f05dac22381
im-adithya <imadithyavardhan@gmail.com>
2023-05-15 20:21:32 +0530

    chore: TS migration

--- src/jupyter/hub/util/jhapiUtil.ts
+++ src/jupyter/hub/util/jhapiUtil.ts
@@ -1,12 +1,16 @@
 const xsrfToken = '';
 
-export const jhapiRequest = (endpoint: string, method: string, data: string) => {
+export const jhapiRequest = (
+  endpoint: string,
+  method: string,
+  data?: Record<string, any>
+): Promise<Response> => {
   //  let api_url = `${base_url}hub/api`;
-  let api_url = 'http://localhost:8000/hub/api';
+  const api_url = 'http://localhost:8000/hub/api';
   let suffix = '';
   if (xsrfToken) {
     // add xsrf token to url parameter
-    var sep = endpoint.indexOf('?') === -1 ? '?' : '&';
+    const sep = endpoint.indexOf('?') === -1 ? '?' : '&';
     suffix = sep + '_xsrf=' + xsrfToken;
   }
   return fetch(api_url + endpoint + suffix, {
@@ -16,7 +20,7 @@ export const jhapiRequest = (endpoint: string, method: string, data: string) => 
     cache: 'no-cache',
     headers: {
       Authorization:
-      'token 9280326a088d3a4a058823ce1c607029e7c11be15af2cb69f32cf507a85c27e5',
+        'token 9280326a088d3a4a058823ce1c607029e7c11be15af2cb69f32cf507a85c27e5',
       'Content-Type': 'application/json',
       Accept: 'application/jupyterhub-pagination+json'
     },
--- src/jupyter/hub/util/withAPI.ts
+++ src/jupyter/hub/util/withAPI.ts
@@ -2,52 +2,52 @@ import { withProps } from 'react-recompose';
 import { jhapiRequest } from './jhapiUtil';
 
 const withAPI = withProps(() => ({
-  updateUsers: (offset, limit, name_filter) =>
+  updateUsers: (offset: number, limit: number, name_filter: string) =>
     jhapiRequest(
       `/users?include_stopped_servers&offset=${offset}&limit=${limit}&name_filter=${
         name_filter || ''
       }`,
       'GET'
     ).then(data => data.json()),
-  updateGroups: (offset, limit) =>
+  updateGroups: (offset: number, limit: number) =>
     jhapiRequest(`/groups?offset=${offset}&limit=${limit}`, 'GET').then(data =>
       data.json()
     ),
   shutdownHub: () => jhapiRequest('/shutdown', 'POST'),
-  startServer: (name, serverName = '') =>
+  startServer: (name: string, serverName = '') =>
     jhapiRequest('/users/' + name + '/servers/' + (serverName || ''), 'POST'),
-  stopServer: (name, serverName = '') =>
+  stopServer: (name: string, serverName = '') =>
     jhapiRequest('/users/' + name + '/servers/' + (serverName || ''), 'DELETE'),
-  startAll: names =>
-    names.map(e => jhapiRequest('/users/' + e + '/server', 'POST')),
-  stopAll: names =>
-    names.map(e => jhapiRequest('/users/' + e + '/server', 'DELETE')),
-  addToGroup: (users, groupname) =>
+  startAll: (names: string[]) =>
+    names.map((e: string) => jhapiRequest('/users/' + e + '/server', 'POST')),
+  stopAll: (names: string[]) =>
+    names.map((e: string) => jhapiRequest('/users/' + e + '/server', 'DELETE')),
+  addToGroup: (users: string[], groupname: string) =>
     jhapiRequest('/groups/' + groupname + '/users', 'POST', { users }),
-  updateProp: (propobject, groupname) =>
+  updateProp: (propobject: Record<string, string>, groupname: string) =>
     jhapiRequest('/groups/' + groupname + '/properties', 'PUT', propobject),
-  removeFromGroup: (users, groupname) =>
+  removeFromGroup: (users: string[], groupname: string) =>
     jhapiRequest('/groups/' + groupname + '/users', 'DELETE', { users }),
-  createGroup: groupName => jhapiRequest('/groups/' + groupName, 'POST'),
-  deleteGroup: name => jhapiRequest('/groups/' + name, 'DELETE'),
-  UserAdds: (usernames, admin) =>
+  createGroup: (groupName: string) =>
+    jhapiRequest('/groups/' + groupName, 'POST'),
+  deleteGroup: (name: string) => jhapiRequest('/groups/' + name, 'DELETE'),
+  UserAdds: (usernames: string[], admin: boolean) =>
     jhapiRequest('/users', 'POST', { usernames, admin }),
-  UserEdit: (username, updated_username, admin) =>
+  UserEdit: (username: string, updated_username: string, admin: boolean) =>
     jhapiRequest('/users/' + username, 'PATCH', {
       name: updated_username,
       admin
     }),
-  deleteUser: username => jhapiRequest('/users/' + username, 'DELETE'),
-  findUser: username => jhapiRequest('/users/' + username, 'GET'),
-  validateUser: username =>
+  deleteUser: (username: string) =>
+    jhapiRequest('/users/' + username, 'DELETE'),
+  findUser: (username: string) => jhapiRequest('/users/' + username, 'GET'),
+  validateUser: (username: string) =>
     jhapiRequest('/users/' + username, 'GET')
       .then(data => data.status)
       .then(data => (data > 200 ? false : true)),
-
   noChangeEvent: () => {
     return null;
   },
-  //
   refreshGroupsData: () =>
     jhapiRequest('/groups', 'GET').then(data => data.json()),
   refreshUserData: () => jhapiRequest('/users', 'GET').then(data => data.json())
