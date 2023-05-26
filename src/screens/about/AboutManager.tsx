05482fb8ab6dc3e505f31eb7cfdee1dd2f233358
Eric Charles <eric@datalayer.io>
2023-05-26 13:58:40 +0300

    chore: about page

--- src/screens/about/AboutManager.tsx
+++ src/screens/about/AboutManager.tsx
@@ -1,30 +1,24 @@
 import { Box, Link, Pagehead } from '@primer/react';
-import { DatalayerIcon, JupyterIcon } from '@datalayer/icons-react/solid';
+import { DatalayerGreenIcon, JupyterIcon } from '@datalayer/icons-react/solid';
 
 const AboutManager = (): JSX.Element => {
   return (
-    <>
+    <Box p={3}>
       <Pagehead as="h1">
           🪐 📋 Jupyter Manager
       </Pagehead>
-      <Box p={3}>
-        <Box as="p">
-          Jupyter Manager is a user interface to configure, monitor and operate your Jupyter platform.
-        </Box>
+      <Box>
         <Box>
-          <JupyterIcon size="large" colored/>
-        </Box>
-        <Box>
-          "Jupyter" is a trademark of the NumFOCUS foundation, of which Project Jupyter is a part.
+          Jupyter Manager is a user interface to configure, monitor and operate your Jupyter platform and is developed with ❤️ by these <Link href="https://github.com/datalayer/jupyter-manager/graphs/contributors">wonderfull contributors</Link>.
         </Box>
         <Box pt={3}>
-          Jupyter Manager is developed with ❤️ by these <Link href="https://github.com/datalayer/jupyter-manager/graphs/contributors">wonderfull contributors</Link>.
+          <DatalayerGreenIcon colored/> Copyright © 2023 Datalayer, Inc.
         </Box>
         <Box pt={3}>
-          <DatalayerIcon colored/> Copyright © 2023 Datalayer, Inc.
+          <JupyterIcon colored/> Jupyter is a trademark of the NumFOCUS foundation, of which Project Jupyter is a part.
         </Box>
       </Box>
-    </>
+    </Box>
   )
 }
 
